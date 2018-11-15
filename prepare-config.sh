5b9d7bdccd319308bf78c373d120023dd34ab976
Francesco Landoni <francescolandoni@gmail.com>
2018-11-15 21:34:20 +0100

    Code style

--- prepare-config.sh
+++ prepare-config.sh
@@ -8,20 +8,20 @@ TOML_PATH="/traefik.toml"
 # TOML_PATH="traefik.toml"
 
 
-ACME=$(jq ".acme" $CONFIG_PATH)
-FILE=$(jq  ".file" $CONFIG_PATH)
-BASIC_AUTH=$(jq --raw-output  ".basicAuth" $CONFIG_PATH)
+ACME=$(jq ".acme" ${CONFIG_PATH})
+FILE=$(jq  ".file" ${CONFIG_PATH})
+BASIC_AUTH=$(jq --raw-output  ".basicAuth" ${CONFIG_PATH})
 ACME_ENABLED=$(echo $ACME | jq --raw-output ".enabled")
-FILE_LENGTH=$(jq  ".file | length" $CONFIG_PATH)
+FILE_LENGTH=$(jq  ".file | length" ${CONFIG_PATH})
 ADDITIONAL_TOML_ACME=""
 ADDITIONAL_TOML_FILE_FRONTENDS=""
 ADDITIONAL_TOML_FILE_BACKENDS=""
-if [ "$ACME_ENABLED" == "true" ]; then
-    acme_email=$(echo $ACME | jq --raw-output ".email")
-    acme_storage=$(echo $ACME | jq --raw-output ".storage")
-    acme_caServer=$(echo $ACME | jq --raw-output ".caServer")
-    acme_entryPoint=$(echo $ACME | jq --raw-output ".entryPoint")
-    acme_onHostRule=$(echo $ACME | jq --raw-output ".onHostRule")
+if [[ "${ACME_ENABLED}" == "true" ]]; then
+    acme_email=$(echo ${ACME} | jq --raw-output ".email")
+    acme_storage=$(echo ${ACME} | jq --raw-output ".storage")
+    acme_caServer=$(echo ${ACME} | jq --raw-output ".caServer")
+    acme_entryPoint=$(echo ${ACME} | jq --raw-output ".entryPoint")
+    acme_onHostRule=$(echo ${ACME} | jq --raw-output ".onHostRule")
 
     ADDITIONAL_TOML_ACME="${ADDITIONAL_TOML_ACME}\n[acme]";
     ADDITIONAL_TOML_ACME="${ADDITIONAL_TOML_ACME}\n  email = \"${acme_email}\"";
@@ -37,11 +37,11 @@ ADDITIONAL_TOML_FILE_FRONTENDS="${ADDITIONAL_TOML_FILE_FRONTENDS}\n[frontends]";
 ADDITIONAL_TOML_FILE_BACKENDS="${ADDITIONAL_TOML_FILE_BACKENDS}\n[backends]";
 for i in `seq 0 $((FILE_LENGTH-1))`;
 do
-    file_item=$(echo $FILE | jq ".[$i]")
-    file_item_id=$(echo $file_item | jq --raw-output ".id")
-    file_item_entryPoint=$(echo $file_item | jq --raw-output ".id")
-    file_item_host=$(echo $file_item | jq --raw-output ".host")
-    file_item_url=$(echo $file_item | jq --raw-output ".url")
+    file_item=$(echo ${FILE} | jq ".[$i]")
+    file_item_id=$(echo ${file_item} | jq --raw-output ".id")
+    file_item_entryPoint=$(echo ${file_item} | jq --raw-output ".id")
+    file_item_host=$(echo ${file_item} | jq --raw-output ".host")
+    file_item_url=$(echo ${file_item} | jq --raw-output ".url")
 
     ADDITIONAL_TOML_FILE_FRONTENDS="${ADDITIONAL_TOML_FILE_FRONTENDS}\n  [frontends.$file_item_id]";
     ADDITIONAL_TOML_FILE_FRONTENDS="${ADDITIONAL_TOML_FILE_FRONTENDS}\n    backend = \"$file_item_id\"";
@@ -53,7 +53,7 @@ do
     ADDITIONAL_TOML_FILE_BACKENDS="${ADDITIONAL_TOML_FILE_BACKENDS}\n    [backends.$file_item_id.servers.$file_item_id]";
     ADDITIONAL_TOML_FILE_BACKENDS="${ADDITIONAL_TOML_FILE_BACKENDS}\n      url = \"$file_item_url\"";
 
-    if [ "$ACME_ENABLED" == "true" ]; then
+    if [[ "$ACME_ENABLED" == "true" ]]; then
         ADDITIONAL_TOML_ACME="${ADDITIONAL_TOML_ACME}\n[[acme.domains]]";
         ADDITIONAL_TOML_ACME="${ADDITIONAL_TOML_ACME}\n  main = \"$file_item_host\"";
     fi
@@ -62,8 +62,8 @@ ADDITIONAL_TOML_FILE="${ADDITIONAL_TOML_FILE}\n${ADDITIONAL_TOML_FILE_FRONTENDS}
 ADDITIONAL_TOML_FILE="${ADDITIONAL_TOML_FILE}\n${ADDITIONAL_TOML_FILE_BACKENDS}";
 
 
-printf "$ADDITIONAL_TOML_ACME" >> $TOML_PATH
-printf "$ADDITIONAL_TOML_FILE" >> $TOML_PATH
+printf "$ADDITIONAL_TOML_ACME" >> ${TOML_PATH}
+printf "$ADDITIONAL_TOML_FILE" >> ${TOML_PATH}
 
 
 
